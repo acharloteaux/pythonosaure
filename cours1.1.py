a61cea8685c29a6ae6f7036d1148f49c30d4cd71
Charloteaux Anne-Laure <annelaure.charloteaux@viacesi.fr>
2025-01-09 12:02:25 +0100

    Suppression des fichiers inutiles

--- cours1.1.py
+++
@@ -1,5 +0,0 @@
-from math import *
-hauteur =int(input ("hauteur = "))
-rayon =int(input ("rayon = "))
-resultat = (pi * rayon * rayon * hauteur)/3
-print("le volume est de" , resultat)
--- cours1.py
+++
@@ -1,3 +0,0 @@
-PrixHT = int(input ("prix ="))
-PrixTTC = PrixHT * 1.20
-print ("prix toute taxtes comprise=", PrixTTC)
